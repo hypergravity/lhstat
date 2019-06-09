0b35c92a51d34274c52b4e688018f1154f58dfcf
Bo Zhang <bozhang@nao.cas.cn>
2019-06-09 22:22:52 +0800

    tweaks

--- update_data_ecs.sh
+++ update_data_ecs.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 cd /root/lhstat/
 rm ./latest_data/*
-scp -p 10415 root@159.226.170.49:/data/lh/sqm/weather2019.csv ./latest_data/
-scp -p 10415 root@159.226.170.49:/data/lh/sqm/SQMReadings.txt ./latest_data/
-scp -p 10415 root@159.226.170.49:/data/lh/sqm/Seeing_Data.txt ./latest_data/
+scp root@159.226.170.49:/data/lh/sqm/weather2019.csv ./latest_data/
+scp root@159.226.170.49:/data/lh/sqm/SQMReadings.txt ./latest_data/
+scp root@159.226.170.49:/data/lh/seeing/Seeing_Data.txt ./latest_data/
