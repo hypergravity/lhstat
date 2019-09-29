fa85d89414eb5f77a3684ec38eabd8b06ca44a29
Bo Zhang <bozhang@nao.cas.cn>
2019-09-29 15:40:40 +0800

    added whitelist in update routine

--- update_data_ecs.sh
+++ update_data_ecs.sh
@@ -3,4 +3,5 @@ cd /root/lhstat/
 rm ./latest_data/*
 scp root@159.226.170.49:/data/lh/sqm/weather2019.csv ./latest_data/
 scp root@159.226.170.49:/data/lh/sqm/SQMReadings.txt ./latest_data/
+scp root@159.226.170.49:/data/lh/sqm/whitelist ./latest_data/
 scp root@159.226.170.49:/data/lh/seeing/Seeing_Data.txt ./latest_data/
