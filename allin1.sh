fd8bffcc89472494eb63718ab924ef7b349e7e64
Bo Zhang <bozhang@nao.cas.cn>
2019-11-30 23:22:03 +0800

    added tails of latest data to log file

--- allin1.sh
+++ allin1.sh
@@ -7,9 +7,10 @@ cd /root/lhstat
 bash update_data_ecs.sh
 
 # do stats
-ipython dostats.py > ./figs/`date +%Y%m%d`/`date +%Y%m%d`.log
+ipython dostats.py
 
 # save a copy for today
 mkdir -p ./figs/`date +%Y%m%d`
 cp ./figs/*.png ./figs/`date +%Y%m%d`/
 cp ./latest_data/* ./figs/`date +%Y%m%d`/
+tail ./latest_data/* ./log/`date +%Y%m%d`.log
