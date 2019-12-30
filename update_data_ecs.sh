b712f9c77ba2367c75582fbf58cec7c5b5c21733
Bo Zhang <bozhang@nao.cas.cn>
2019-12-30 20:27:55 +0800

    added dust data in sync

--- update_data_ecs.sh
+++ update_data_ecs.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 cd /root/lhstat/
 rm ./latest_data/*
-scp root@159.226.170.49:/data/lh/sqm/weather2019.csv ./latest_data/
-scp root@159.226.170.49:/data/lh/sqm/SQMReadings.txt ./latest_data/
-scp root@159.226.170.49:/data/lh/sqm/whitelist ./latest_data/
-scp root@159.226.170.49:/data/lh/seeing/Seeing_Data.txt ./latest_data/
+rsync -avz root@159.226.170.49:/data/lh/sqm/weather2019.csv ./latest_data/
+rsync -avz root@159.226.170.49:/data/lh/sqm/SQMReadings.txt ./latest_data/
+rsync -avz root@159.226.170.49:/data/lh/sqm/whitelist ./latest_data/
+rsync -avz root@159.226.170.49:/data/lh/seeing/Seeing_Data.txt ./latest_data/
+rsync -avz root@159.226.170.49:/data/lh/seeing/Seeing_Data.txt ./latest_data/
+rsync -avrz root@159.226.170.49:/data/lh/dust ./latest_data/
