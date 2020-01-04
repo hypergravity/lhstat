c67c5486683cef8ce35e41410f25f6cadea3aa04
Bo Zhang <bozhang@nao.cas.cn>
2020-01-04 23:21:05 +0800

    sync old sqm data

--- update_data_ecs.sh
+++ update_data_ecs.sh
@@ -3,6 +3,7 @@ cd /root/lhstat/
 #rm ./latest_data/*
 rsync -avz root@159.226.170.49:/data/lh/sqm/weather2019.csv ./latest_data/
 rsync -avz root@159.226.170.49:/data/lh/sqm/SQMReadings.txt ./latest_data/
+rsync -avz root@159.226.170.49:/data/lh/sqm/SQMReadings_20180923.txt ./latest_data/
 rsync -avz root@159.226.170.49:/data/lh/sqm/whitelist ./latest_data/
 rsync -avz root@159.226.170.49:/data/lh/seeing/Seeing_Data.txt ./latest_data/
 rsync -avz root@159.226.170.49:/data/lh/seeing/Seeing_Data.txt ./latest_data/
