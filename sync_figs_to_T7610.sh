16c7198e5417396f65962c104d10acccd5336b57
Bo Zhang <bozhang@nao.cas.cn>
2020-03-08 11:09:13 +0800

    sync figs to T7610 using rysnc

--- sync_figs_to_T7610.sh
+++ sync_figs_to_T7610.sh
@@ -1,2 +1,2 @@
 #scp -P 10415 -r root@101.201.57.57:/root/lhstat/figs/* /home/cham/PycharmProjects/lhstat/figs/
-scp -P 10284 -r root@ucas.china-vo.org:~/lhstat/figs/* /home/cham/PycharmProjects/lhstat/figs/
+rsync -e 'ssh -p 10284' -avzr root@ucas.china-vo.org:~/lhstat/figs/* ~/PycharmProjects/lhstat/figs/
