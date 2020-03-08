626ce6cfeae0528da4a49c0335930b3a97e09e7d
Bo Zhang <bozhang@nao.cas.cn>
2020-03-08 20:52:01 +0800

    added log directory

--- update_data_ecs.sh
+++ update_data_ecs.sh
@@ -8,3 +8,4 @@ rsync -avz root@159.226.170.49:/data/lh/sqm/whitelist ./latest_data/
 rsync -avz root@159.226.170.49:/data/lh/seeing/Seeing_Data.txt ./latest_data/
 rsync -avz root@159.226.170.49:/data/lh/seeing/Seeing_Data.txt ./latest_data/
 rsync -avrz root@159.226.170.49:/data/lh/dust ./latest_data/
+rsync -avrz root@159.226.170.49:/data/lh/lhdust02.dat ./latest_data/
