4e5fbf470495a8fb4d109043cb4586d83952aa16
Bo Zhang <bozhang@nao.cas.cn>
2020-04-23 21:43:02 +0800

    updated server to cloudvm

--- sync_data_to_local.sh
+++ sync_data_to_local.sh
@@ -1,4 +1,5 @@
 #rsync -avzr --port=10415 root@101.201.57.57:/root/lhstat/latest_data /home/cham/PycharmProjects/lhstat/
 #rsync -avzr --port=10284 root@ucas.china-vo.org:/root/lhstat/latest_data /home/cham/PycharmProjects/lhstat/
 #scp -P 10284 -r root@ucas.china-vo.org:/root/lhstat/latest_data /home/cham/PycharmProjects/lhstat/
-rsync -e 'ssh -p 10284' -avzr root@ucas.china-vo.org:/root/lhstat/latest_data ~/PycharmProjects/lhstat/
+#rsync -e 'ssh -p 10284' -avzr root@ucas.china-vo.org:/root/lhstat/latest_data ~/PycharmProjects/lhstat/
+rsync -e 'ssh -p 13053' -avzr root@cloudvm.china-vo.org:/root/lhstat/latest_data ~/PycharmProjects/lhstat/
--- sync_figs_to_local.sh
+++ sync_figs_to_local.sh
@@ -1,2 +1,3 @@
 #scp -P 10415 -r root@101.201.57.57:/root/lhstat/figs/* /home/cham/PycharmProjects/lhstat/figs/
-rsync -e 'ssh -p 10284' -avzr root@ucas.china-vo.org:~/lhstat/figs/* ~/PycharmProjects/lhstat/figs/
+#rsync -e 'ssh -p 10284' -avzr root@ucas.china-vo.org:~/lhstat/figs/* ~/PycharmProjects/lhstat/figs/
+rsync -e 'ssh -p 13053' -avzr root@cloudvm.china-vo.org:~/lhstat/figs/* ~/PycharmProjects/lhstat/figs/
