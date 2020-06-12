b59c466ccf1595b0c2f06cce1392126f3585677b
Bo Zhang <bozhang@nao.cas.cn>
2020-06-12 10:38:27 +0800

    updated grabcam.py: added date choice

--- lhstat/grabcam.py
+++ lhstat/grabcam.py
@@ -100,11 +100,19 @@ if __name__ == "__main__":
     os.mkdir(dir_temp)
 
     # today, noon
-    dt_today = datetime.now()
-    t_today = Time("{:04d}-{:02d}-{:02d}T12:00:00".format(
-        dt_today.year, dt_today.month, dt_today.day), format="isot")
-    t_yesterday = t_today- TimeDelta(1, format="jd")
-    dt_yesterday = t_yesterday.to_datetime()
+    import sys
+    if len(sys.argv) == 1:
+        dt_today = datetime.now()
+        t_today = Time("{:04d}-{:02d}-{:02d}T12:00:00".format(
+            dt_today.year, dt_today.month, dt_today.day), format="isot")
+        t_yesterday = t_today- TimeDelta(1, format="jd")
+        dt_yesterday = t_yesterday.to_datetime()
+    else:
+        dt_today = sys.argv[1]
+        t_today = Time("{}-{}-{}T12:00:00".format(
+            dt_today[0:4], dt_today[4:6], dt_today[6:8]), format="isot")
+        t_yesterday = t_today - TimeDelta(1, format="jd")
+        dt_yesterday = t_yesterday.to_datetime()
 
     # jd of ev & momonts of last night
     from bisect import bisect
