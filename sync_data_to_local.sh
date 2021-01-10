c7461e459a67a04e4d49c3fead7a70735d1c10e0
Bo Zhang <bozhang@nao.cas.cn>
2021-01-10 17:38:12 +0800

    updated port number

--- sync_data_to_local.sh
+++ sync_data_to_local.sh
@@ -2,4 +2,4 @@
 #rsync -avzr --port=10284 root@ucas.china-vo.org:/root/lhstat/latest_data /home/cham/PycharmProjects/lhstat/
 #scp -P 10284 -r root@ucas.china-vo.org:/root/lhstat/latest_data /home/cham/PycharmProjects/lhstat/
 #rsync -e 'ssh -p 10284' -avzr root@ucas.china-vo.org:/root/lhstat/latest_data ~/PycharmProjects/lhstat/
-rsync -e 'ssh -p 13053' -avzr root@cloudvm.china-vo.org:/root/lhstat/latest_data ~/PycharmProjects/lhstat/
+rsync -e 'ssh -p 12898' -avzr root@cloudvm.china-vo.org:/root/lhstat/latest_data ~/PycharmProjects/lhstat/
